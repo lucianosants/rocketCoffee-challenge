5f5e6c058877031d1f6a1524077cd2d6bf191614
lucianosants <lucianola18@gmail.com>
2022-06-18 21:08:32 -0300

    navbar has done

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const menuButton = document.querySelector('.menu-button');
 
 function toggoleMenu() {
 	const navigation = document.querySelector('.navigation');
+	const menuContainer = document.querySelector('.menu-container');
 	navigation.classList.toggle('active');
+	menuContainer.classList.toggle('open');
 }
 
 menuButton.addEventListener('click', toggoleMenu);
